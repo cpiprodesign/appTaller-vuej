90c78486bfd3ee170ab307886730c1ae5eb4539b
Cristian <cristian@cpiprodesig.com>
2022-09-08 21:05:35 -0500

    add-seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,11 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+// use App\Models\Cliente;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +25,17 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        // $usuario = new User();
+        // $usuario->usuario="admin";
+        // $usuario->password='$2y$10$CD10K3aY2qqzh0vzqmREs.8lrWQgoxN97lQJxWBm6vYtbmK0N47rK';
+        // $usuario->condicion="1";
+        // $usuario->save();
+        DB::table('users')->insert([
+            'usuario' => 'admin',
+            //'password' => Str::random(10).'@gmail.com',
+            'password' => Hash::make('password'),
+            'condicion' => (1),
+        ]);
+
     }
 }
